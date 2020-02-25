18749a7fcff48efbbcb0d6ff681ad7c96b4adc3d
TNice <tylernicewarner@yahoo.com>
2020-02-25 16:20:59 -0600

    Fixed dynamc connection

--- mqtt/virtual_sensor.py
+++ mqtt/virtual_sensor.py
@@ -9,20 +9,22 @@ import paho.mqtt.client as mqtt
 import time
 import random as rand
 
-deviceID = "vs001"
-pollingRate = 1 #number of reads each sensor has per second
+deviceID = "vs002"
+pollingRate = 0.5 #number of reads each sensor has per second
 running = True
 
-def read_sensors():
-    global running
-    while running:
-        timestamp = time.time()
-        for j in range(10):
-            message = deviceID + "," + str(timestamp) + "," + str(j) + "," + str((rand.randrange(1, 200, 3) * 3.14159)  % 20)
-            client.publish("/" + deviceID + "/data", message)
-            print(message)
+client = mqtt.Client(client_id="v_sens000")
+client.username_pw_set("mqtsqrfd", "qWsfSyHmt1D-")
 
-        time.sleep((1 / pollingRate) - (time.time() - timestamp)) #polling rate - time from 1st to last read
+def read_sensors():
+    global client
+    print(client.is_connected())
+    #polling rate - time from 1st to last read
+    timestamp = time.time()
+    for j in range(10):
+        message = deviceID + "," + str(timestamp) + "," + str(j) + "," + str((rand.randrange(1, 200, 3) * 3.14159)  % 20)
+        client.publish("/" + deviceID + "/data", message)
+        print(message)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -35,21 +37,23 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    global running
     command = str(msg.payload).replace('\'', '').replace('b', '', 1).lower()
-    if command == "stop":
-        running = False
-    elif command == "start":
-        running = True
-        print("Starting Up")
+    if command == "get" or command == "start":
         read_sensors()
 
-client = mqtt.Client(client_id="v_sens000")
-client.username_pw_set("mqtsqrfd", "qWsfSyHmt1D-")
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("hairdresser.cloudmqtt.com", 17102, 60)    
+
+#while running:
+#    timestamp = time.time()
+#    for j in range(10):
+#        message = deviceID + "," + str(timestamp) + "," + str(j) + "," + str((rand.randrange(1, 200, 3) * 3.14159)  % 20)
+#        client.publish("/data", message)
+#        print(message)
+#    time.sleep((1 / pollingRate) - (time.time() - timestamp))
+
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
