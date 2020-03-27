0b5b8b2dae809993de44d577daeefcca636c34d0
Felipe Costa <felipevalois_@hotmail.com>
2020-03-26 19:57:16 -0500

    Add columns and color sensors

--- capstone2/src/app/balance/balance.component.ts
+++ capstone2/src/app/balance/balance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api/api.service';
 
 @Component({
   selector: 'app-balance',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BalanceComponent implements OnInit {
 
-  constructor() { }
+  sensors: any[];
+
+  constructor(private api: ApiService) {}
 
   ngOnInit() {
+    this.getSensors();
   }
+
+  getSensors(){
+    this.api.getSensors().subscribe(res => {
+      console.log(res);
+      res = Object.values(res);
+      this.sensors = res.map(function(value){
+        return parseFloat(parseFloat(value).toPrecision(3));
+      });
+      console.log(this.sensors);
+    });
+  }
+
+  colorSensor(sensor){
+    if(sensor>=0 && sensor<=5) return 0;
+    else if(sensor>5 && sensor<=15) return 1;
+    else return 2;
+  }
+
+
 
 }
