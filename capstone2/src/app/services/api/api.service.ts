1c0966a0389ff11d4493c22bf9a9a69ca378eab1
Albert Klorer <albertklorer@Alberts-MBP.attlocal.net>
2020-04-14 00:20:04 -0500

    allowed API service to call either vs001 or vs002

--- capstone2/src/app/services/api/api.service.ts
+++ capstone2/src/app/services/api/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -17,7 +18,13 @@ export class ApiService {
     constructor(private httpClient: HttpClient) {}
 
     public getSensors(){
-        return this.httpClient.get<any>('https://cors-anywhere.herokuapp.com/http://ec2-34-219-51-110.us-west-2.compute.amazonaws.com:8080/vs002', this.httpOptions);
+        if (environment.sensorID == 'vs002') {
+            return this.httpClient.get<any>('https://cors-anywhere.herokuapp.com/http://ec2-34-219-51-110.us-west-2.compute.amazonaws.com:8080/vs002', this.httpOptions);
+        }
+
+        else if (environment.sensorID == 'vs001') {
+            return this.httpClient.get<any>('https://cors-anywhere.herokuapp.com/http://ec2-34-219-51-110.us-west-2.compute.amazonaws.com:8080/vs001', this.httpOptions);
+        }
     }
 
 
