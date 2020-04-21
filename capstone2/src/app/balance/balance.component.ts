eadb028365362ec367aa24cb7c37f05f628dbbe6
aceprogram <calexander618@gmail.com>
2020-04-20 20:29:20 -0500

    balance functionality working

--- capstone2/src/app/balance/balance.component.ts
+++ capstone2/src/app/balance/balance.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api/api.service';
 import { NgxSpinnerService } from "ngx-spinner";
-import { environment } from 'src/environments/environment';
+
+class CircuitData {
+  circuit1: number[];
+  circuit2: number[];
+  circuitUsage: number[] = [0.0, 0.0];
+}
 
 @Component({
   selector: 'app-balance',
@@ -9,34 +14,83 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./balance.component.css']
 })
 export class BalanceComponent implements OnInit {
-  sensors: any[];
-
+  data: CircuitData;
+  savings: number = 0.0;
+  balanced: boolean = false;
   constructor(
     private api: ApiService,
     private spinner: NgxSpinnerService
   ) { }
 
   ngOnInit() {
+    this.data = new CircuitData();
     this.getSensors();
   }
 
-  getSensors() {
+  getSensors(): void {
     this.spinner.show();
     this.api.getSensors().subscribe(res => {
       this.spinner.hide();
       res = Object.values(res);
-      this.sensors = res.map(function (value) {
+      let sensors: number[] = res.map(function (value) {
         return parseFloat(parseFloat(value).toPrecision(3));
       });
+      this.data.circuit1 = sensors.slice(0, sensors.length/2);
+      this.data.circuit2 = sensors.slice(sensors.length/2, sensors.length);
+
+      this.calculateCircuitUsage(this.data);
+      this.savings = this.calculateSavings(this.data);
     });
   }
+  calculateCircuitUsage(data: CircuitData) {
+    for (var i = 0; i < data.circuit1.length; i++) {
+      data.circuitUsage[0] += data.circuit1[i];
+    }
+    for (var i = 0; i < data.circuit2.length; i++) {
+      data.circuitUsage[1] += data.circuit2[i];
+    }
+  }
 
-  colorSensor(sensor) {
+  calculateSavings(data: CircuitData): number {
+    let tempData: CircuitData = JSON.parse(JSON.stringify(data));
+
+    this.balanceSensors(tempData);
+
+    tempData.circuitUsage = [0.0, 0.0];
+    this.calculateCircuitUsage(tempData);
+    
+    return Math.abs(data.circuitUsage[0] - data.circuitUsage[1]) - Math.abs(tempData.circuitUsage[0] - tempData.circuitUsage[1]);
+  }
+
+  colorSensor(sensor: number): number {
     if (sensor >= 0 && sensor <= 5) return 0;
     else if (sensor > 5 && sensor <= 15) return 1;
     else return 2;
   }
 
+  balanceSensors(data: CircuitData): CircuitData {
+    let sensors = data.circuit1.concat(data.circuit2);
+    sensors.sort(function (a, b) { return b > a ? 1 : -1 });
 
+    let tempCircuit1 = [];
+    let tempCircuit2 = [];
+    let tempCircuitUsage = [0.0, 0.0];
+    for (var i = 0; i < 10; i++) {
+      if (tempCircuitUsage[0] > tempCircuitUsage[1]) {
+        tempCircuitUsage[1] += sensors[i];
+        tempCircuit2.push(sensors[i]);
+      }
+      else {
+        tempCircuitUsage[0] += sensors[i];
+        tempCircuit1.push(sensors[i]);
+      }
+    }
+    data.circuit1 = tempCircuit1;
+    data.circuit2 = tempCircuit2;
+    data.circuitUsage = tempCircuitUsage;
 
+    this.savings = 0;
+
+    return data;
+  }
 }
