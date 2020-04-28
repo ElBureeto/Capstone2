fdde2b9ac4b2f1036448462e6ba1979959226cef
Albert Klorer <albertklorer@Alberts-MBP.attlocal.net>
2020-04-28 14:31:36 -0500

    removed HTTP options

--- capstone2/src/app/services/api/api.service.ts
+++ capstone2/src/app/services/api/api.service.ts
@@ -7,23 +7,16 @@ import { environment } from 'src/environments/environment';
 })
 
 export class ApiService {
-    
-    httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        })
-    };
 
     constructor(private httpClient: HttpClient) {}
 
     public getSensors(){
         if (environment.sensorID == 'vs002') {
-            return this.httpClient.get<any>('https://cors-anywhere.herokuapp.com/http://ec2-34-219-51-110.us-west-2.compute.amazonaws.com:8080/vs002', this.httpOptions);
+            return this.httpClient.get<any>('http://ec2-34-219-51-110.us-west-2.compute.amazonaws.com:8080/vs002', this.httpOptions);
         }
 
         else if (environment.sensorID == 'vs001') {
-            return this.httpClient.get<any>('https://cors-anywhere.herokuapp.com/http://ec2-34-219-51-110.us-west-2.compute.amazonaws.com:8080/vs001', this.httpOptions);
+            return this.httpClient.get<any>('http://ec2-34-219-51-110.us-west-2.compute.amazonaws.com:8080/vs001', this.httpOptions);
         }
     }
 
